5d228894d56eff63bccb1c1d270531b13cf2eae9
btoddeducation <165634150+btoddeducation@users.noreply.github.com>
2024-05-05 16:56:24 -1000

    test

--- script.js
+++
@@ -1,8 +0,0 @@
-function displaySkills(champion) {
-    var championDiv = document.getElementById(champion);
-    if (championDiv.classList.contains("hidden")) {
-        championDiv.classList.remove("hidden");
-    } else {
-        championDiv.classList.add("hidden");
-    }
-}
